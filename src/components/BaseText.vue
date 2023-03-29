ba9119f74a545e16f0343a1b9e08324f02e21296
Aungsittkhaing <aungsittkhaing695@gmail.com>
2023-03-29 19:52:33 +0630

    to do list

---
+++ src/components/BaseText.vue
@@ -0,0 +1,14 @@
+<template>
+  <span><slot></slot></span>
+</template>
+
+<script>
+export default {
+  name: "BaseText",
+  // props : ["text"]
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "animate.css/animate.css"
+import BaseText from "./components/BaseText";
 
+Vue.component("BaseText", BaseText)
 Vue.config.productionTip = false
 
 new Vue({
